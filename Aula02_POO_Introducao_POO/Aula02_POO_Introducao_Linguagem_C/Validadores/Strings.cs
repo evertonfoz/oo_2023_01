cb818b08a48720ab73582f79dcd5c6cf2168a68c
Everton Coimbra de Araújo <evertoncoimbra@gmail.com>
2022-03-22 13:19:13 -0300

    Aula 02

---
+++ Aula02_POO_Introducao_POO/Aula02_POO_Introducao_Linguagem_C/Validadores/Strings.cs
@@ -0,0 +1,10 @@
+﻿namespace Aula02_POO_Introducao_Linguagem_C.Validadores
+{
+    internal class Strings
+    {
+        public static bool LengthEquals(string texto, int length)
+        {
+            return texto.Length == length;
+        }
+    }
+}
---
+++ Aula03_POO_Associacoes/Aula02_POO_Introducao_Linguagem_C/Validadores/ValidadoresNumericos.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Aula02_POO_Introducao_Linguagem_C.Validadores
+{
+    internal class ValidadoresNumericos
+    {
+        public static bool EhNumerico(string valor)
+        {
+            Regex regex = new Regex(@"^-?[0-9][0-9,\.]+$");
+                return regex.IsMatch(valor);
+        }
+    }
+}
