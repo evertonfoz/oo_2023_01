7d439199803a725e0db6e6884cc129f80ed73920
Everton Coimbra de Araújo <evertoncoimbra@gmail.com>
2022-08-23 09:30:43 -0300

    Aula do dia 23

---
+++ 2022_02/Aula02/Domain/Student.cs
@@ -0,0 +1,74 @@
+﻿namespace Aula01.Domain
+{
+    internal class Student
+    {
+        #region Props Full
+        private int _studentID;
+
+        public int StudentID
+        {
+            get { return _studentID; }
+            private set {
+                _studentID = value;
+            }
+        }
+
+        private string _uuidID;
+
+        public string UuidID
+        {
+            get { return _uuidID; }
+            private set { _uuidID = value; }
+        }
+
+        #endregion
+
+        #region Constructors
+        public Student() {
+            Random rnd = new();
+            StudentID = rnd.Next(10, 20);
+            Guid newGuid = Guid.NewGuid();
+            UuidID = newGuid.ToString();
+        }
+        #endregion
+
+        //#region C# Properties
+        //public int StudentID
+        //{
+        //    set
+        //    {
+        //        if (value < 0)
+        //        {
+        //            throw new ArgumentException();
+        //        }
+        //    }
+        //}
+        //#endregion
+
+
+        #region Fields
+        //private int _studentID;
+        public string AcademicRegister;
+        public string Name;
+        #endregion
+
+        //#region Properties
+        //public int StudentID()
+        //{
+        //    return _studentID;
+        //}
+        //#endregion
+
+        //#region Methods
+        //public bool RegisterStudentID(int id)
+        //{
+        //    if (id <= 0)
+        //    {
+        //        return false;
+        //    }
+        //    _studentID = id;
+        //    return true;
+        //}
+        //#endregion
+    }
+}
---
+++ 2022_02/Aula02_Exercicio/Dominio/Funcionario.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.ConstrainedExecution;
+
+namespace Aula02_Exercicio.Dominio
+{
+    //2)	Escreva um programa para ler o salário bruto de um 
+    //    funcionário e calcular o desconto dos impostos.Considere 
+    //    que é descontado o INSS de 8% e o vale transporte no valor 
+    //    de R$ 60,00. Ao final o programa deve exibir 
+    //    “O funcionário receberá R$ <valor_calculado> ”. 
+    internal class Funcionario
+    {
+        private double? _salarioBruto;
+
+        public double SalarioBruto
+        {
+            get {
+                _validarSalarioBruto("Valor ainda não informado");
+                return _salarioBruto??0; 
+            }
+            set {
+                _validarSalarioBruto($"{value} inválido", 
+                    valorParaValidar: value);
+                _salarioBruto = value;
+            }
+        }
+
+        public double DescontoINSS { get
+            {
+                _validarSalarioBruto("Salário Bruto ainda não informado");
+                
+                return (_salarioBruto??0) * 0.08;
+            }
+        }
+
+        private void _validarSalarioBruto(string mensagem, 
+            double? valorParaValidar = null
+           )
+        {
+            valorParaValidar = valorParaValidar ?? _salarioBruto;
+            if (valorParaValidar == null || valorParaValidar <= 0)
+            {
+                throw new ArgumentException(mensagem);
+            }
+        }
+    }
+}
